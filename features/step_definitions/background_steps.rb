d5d682c9252b1d30f1fe5334bbfcc7626145dd7b
oliverochman <oliver.ochman@hotmail.com>
2019-12-06 11:11:51 +0100

    update create article feature test for logged in user

--- features/step_definitions/background_steps.rb
+++ features/step_definitions/background_steps.rb
@@ -3,3 +3,15 @@ Given("the following articles exist:") do |table|
     create(:article, article_attrs)
   end
 end
+
+Given("the following users exist:") do |table|
+  table.hashes.each do |user_attrs|
+    create(:user, user_attrs)
+  end
+end
+
+Given("I am logged in as {string}") do |email|
+  user = User.find_by(email: email)
+
+  login_as(user, scope: :user)
+end
